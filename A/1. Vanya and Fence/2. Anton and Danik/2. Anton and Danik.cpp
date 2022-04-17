4b3f99a20285aa44e498296c80cdedf63a7e42ae
hindawi <ahmedhindawi2001@gmail.com>
2022-04-17 07:07:51 +0200

    Anton and Danik

---
+++ A/1. Vanya and Fence/2. Anton and Danik/2. Anton and Danik.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    int numOfGamesPlayed, Anton=0, Danik=0;
+    string s;
+    cin >> numOfGamesPlayed >> s;
+    for (int i = 0; i < numOfGamesPlayed; i++) {
+        if (s[i] == 'A') {
+            Anton++;
+        }
+        else {
+            Danik++;
+        }
+    }
+    if (Anton == Danik) {
+        cout << "Friendship";
+    }
+    else if (Anton > Danik) {
+        cout << "Anton";
+    }
+    else {
+        cout << "Danik";
+    }
+}
