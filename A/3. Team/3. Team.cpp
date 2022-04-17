69c34bee5726402e8cbd50c31ec365ac5d4b09bc
hindawi <ahmedhindawi2001@gmail.com>
2022-04-17 08:58:43 +0200

    Team

---
+++ A/3. Team/3. Team.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	int n, numOfSolvedProblems=0;
+	cin >> n;
+	int P, V, T, sum;
+	for (int i = 1; i <= n; i++) {
+		cin >> P >> V >> T;
+		sum = P + V + T;
+		if (sum >= 2) {
+			numOfSolvedProblems += 1;
+		}
+	}
+	cout << numOfSolvedProblems;
+}
