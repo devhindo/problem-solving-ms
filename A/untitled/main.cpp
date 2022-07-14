b94537da85b8729d74998567323d9144beaea1d2
hindawi <ahmedhindawi2001@gmail.com>
2022-07-14 15:23:36 +0200

    Night at the Museusm

--- A/untitled/main.cpp
+++ A/untitled/main.cpp
@@ -2,8 +2,7 @@
 
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <cstdlib>
+
 
 using namespace std;
 
@@ -11,10 +10,11 @@ int main() {
     string name;
     cin >> name;
     char alphapets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-     int index = find(alphapets, alphapets + 26, 'z') - alphapets;
-    int size = sizeof(alphapets) / sizeof(alphapets[0]);
-    cout << index;
-   
+    int place = 0, numOfMoves = 0;
+    for (int i=0; i<name.length(); i++) {
+        int leftApproach, rightApproach;
+        rightApproach =
+    }
 
 
 
