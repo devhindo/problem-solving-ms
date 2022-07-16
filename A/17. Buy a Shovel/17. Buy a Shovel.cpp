8558b3eb977b4d986b6f8c8b11a3f1e254b35eb2
hindawi <ahmedhindawi2001@gmail.com>
2022-07-16 19:33:12 +0200

    Buy a Shovel

--- A/17. Buy a Shovel/17. Buy a Shovel.cpp
+++ A/17. Buy a Shovel/17. Buy a Shovel.cpp
@@ -1,21 +1,17 @@
 // Link : https://codeforces.com/contest/731/problem/A
 
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <cstdlib>
 
 using namespace std;
 
-int main() {
-    string name;
-    cin >> name;
-    char alphapets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-     int index = find(alphapets, alphapets + 26, 'z') - alphapets;
-    int size = sizeof(alphapets) / sizeof(alphapets[0]);
-    cout << index;
-   
-
-
-
+int main() { // 117 3 => 9 || 15 2 => 2 || 237 7 || 1
+// 9*117 = 1053;
+ int itemPrice, change, numOfItems = 0, totalPrice;
+ cin >> itemPrice >> change;
+do
+{
+    numOfItems++;   
+    totalPrice = itemPrice * numOfItems;
+} while (((totalPrice - change) % 10 != 0) && ((numOfItems*itemPrice) % 10 != 0));
+    cout << numOfItems;
 }
